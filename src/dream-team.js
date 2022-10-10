d2a4fd017dcfd685498cd57ac0c265951856de76
unknown <s.kudlai@mail.ru>
2022-10-10 17:42:50 +0300

    feat: tasks Count cats!, Dream team, Tower of Hanoi

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const NAME_TEAM = members.map(value => {
+    if (typeof value === 'string') return value.trim().at(0).toUpperCase();
+  })
+  const RESULT = NAME_TEAM.sort().join('');
+  return RESULT ? RESULT : false;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+  const DOUBLE_NEXT = '--double-next';
+  const DOUBLE_PREV = '--double-prev';
+  const DISCARD_NEXT = '--discard-next';
+  const DISCARD_PREV = '--discard-prev';
+  const NEW_ARR = [...arr];
+
+  // if (NEW_ARR.includes(DOUBLE_NEXT) && NEW_ARR.includes(DOUBLE_NEXT) && NEW_ARR.includes(DISCARD_NEXT) && DISCARD_PREV) return [];
+
+  const RESULT = NEW_ARR.reduce((acc, value, index, arr) => {
+
+
+
+    if (value === DOUBLE_PREV) {
+      if (index === 0) {
+        arr.splice(index, 1);
+        acc = [...arr];
+      } else {
+        const DOUBLE_PREV = arr[index - 1];
+        arr.splice(index, 1, DOUBLE_PREV);
+        acc = [...arr];
+      }
+    }
+
+    if (value === DOUBLE_NEXT) {
+      if (index === NEW_ARR.length - 1) {
+        arr.splice(index, 1);
+        acc = [...arr];
+      } else {
+        const DOUBLE_NUM = arr[index + 1];
+        arr.splice(index, 1, DOUBLE_NUM);
+        acc = [...arr];
+      }
+    }
+
+    if (value === DISCARD_PREV) {
+      if (index === 0) {
+        arr.splice(index, 1);
+        acc = [...arr];
+      } else {
+        arr.splice(index - 1, 2);
+        acc = [...arr];
+      }
+    }
+
+    if (value === DISCARD_NEXT) {
+      arr.splice(arr.indexOf(value), 2);
+      acc = [...arr];
+    }
+
+
+    if (NEW_ARR.includes(DISCARD_NEXT) && NEW_ARR.includes(DISCARD_PREV)) {
+      const indexNext = NEW_ARR.indexOf(DISCARD_NEXT);
+      const indexPrev = NEW_ARR.indexOf(DISCARD_PREV);
+
+      if (indexNext < indexPrev && indexPrev - indexNext === 2) {
+        const CURENT_INDEX = indexNext + 1;
+        if (value !== DISCARD_NEXT && value !== DISCARD_PREV && value !== NEW_ARR[CURENT_INDEX]) {
+          arr.splice(indexNext, 3)
+          acc = [...arr];
+        }
+      } else {
+        arr.splice(indexPrev - 1, 2);
+        arr.splice(arr.indexOf(DISCARD_NEXT), 2);
+        acc = [...arr];
+      }
+    }
+
+    if (NEW_ARR.includes(DISCARD_NEXT) && NEW_ARR.includes(DOUBLE_PREV)) {
+      const indexNext = NEW_ARR.indexOf(DISCARD_NEXT);
+      const indexDouble = NEW_ARR.indexOf(DOUBLE_PREV);
+
+      if (indexNext < indexDouble && indexDouble - indexNext === 2) {
+        arr.splice(indexNext, 3);
+        acc = [...arr];
+      }
+    }
+
+    return acc;
+  }, []);
+
+  return RESULT;
 }
 
-module.exports = {
-  transform
-};
+
+// module.exports = {
+//   transform
+// };
