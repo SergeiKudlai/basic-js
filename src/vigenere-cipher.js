27fdbdbc81d242fdda1cec944975ca3e7b9facf3
unknown <s.kudlai@mail.ru>
2022-10-13 12:10:58 +0300

    fix: vigenere-cipher.js completed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -33,7 +33,7 @@ class VigenereCipheringMachine {
 
     const OBJ_SYMBOL = {};
 
-    for (let i = 0; i < NEW_MESSAGE.length; i++) {
+    for (let i = 0; i < NEW_MESSAGE.length; i++) { //отделение символов
       if (NEW_MESSAGE[i].charCodeAt(0) < 65 || NEW_MESSAGE[i].charCodeAt(0) > 90) OBJ_SYMBOL[i] = NEW_MESSAGE[i];
     }
 
@@ -48,8 +48,8 @@ class VigenereCipheringMachine {
     const TABLE_OBJ = {}; //таблица
     this.arr.forEach((value, index) => TABLE_OBJ[value] = index);
 
-    const FINNALY_ARR = Array(NEW_MESSAGE.length);
-    const RESULT_ARR = [];
+    const FINNALY_ARR = Array(NEW_MESSAGE.length); //создания выходного массива с добавлением слотов
+    const RESULT_ARR = []; //код на выходе зашифрованный
 
     if (this.valueStart === undefined || this.valueStart === true) {
       for (let i = 0; i < MESSAGE_ARR.length; i++) {
@@ -94,36 +94,69 @@ class VigenereCipheringMachine {
   decrypt(encryptedMessage, key) { //расшифровка
     if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
 
-    encryptedMessage = encryptedMessage.toUpperCase().replace(/ /g, '');
-    key = key.toUpperCase().replace(/ /g, '');
+    const NEW_MESSAGE = encryptedMessage.toUpperCase();
+
+    const OBJ_SYMBOL = {};
+
+    for (let i = 0; i < NEW_MESSAGE.length; i++) { //отделение символов
+      if (NEW_MESSAGE[i].charCodeAt(0) < 65 || NEW_MESSAGE[i].charCodeAt(0) > 90) OBJ_SYMBOL[i] = NEW_MESSAGE[i];
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase().replace(/[^A-Z]/g, '');
+    key = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    const MESSAGE_ARR = [...'' + encryptedMessage];
+
+    const KEY_ARR = [...'' + key.repeat(encryptedMessage.length)];
+    KEY_ARR.length = encryptedMessage.length; //ограничение длины
+
+    const TABLE_OBJ = {}; //таблица
+    this.arr.forEach((value, index) => TABLE_OBJ[value] = index);
+
+    const FINNALY_ARR = Array(NEW_MESSAGE.length);
+    const RESULT_ARR = [];
 
     if (this.valueStart === undefined || this.valueStart === true) {
+      for (let i = 0; i < MESSAGE_ARR.length; i++) {
+        let index = TABLE_OBJ[MESSAGE_ARR[i]] - TABLE_OBJ[KEY_ARR[i]];
+        if (index < 0) index = TABLE_OBJ[MESSAGE_ARR[i]] + this.arr.length - TABLE_OBJ[KEY_ARR[i]];
+        RESULT_ARR.push(this.arr[index]);
+      }
+
+      for (let i = 0, j = 0; i < NEW_MESSAGE.length; i++) {
+        if (OBJ_SYMBOL[`${i}`]) {
+          FINNALY_ARR[i] = OBJ_SYMBOL[i]
+        } else {
+          FINNALY_ARR[i] = RESULT_ARR[j];
+          j++;
+        }
+      }
 
 
-
-
+      return FINNALY_ARR.join('');
 
     } else if (this.valueStart === false) {    // reverse
 
+      for (let i = 0; i < MESSAGE_ARR.length; i++) {
+        let index = TABLE_OBJ[MESSAGE_ARR[i]] - TABLE_OBJ[KEY_ARR[i]];
+        if (index < 0) index = TABLE_OBJ[MESSAGE_ARR[i]] + this.arr.length - TABLE_OBJ[KEY_ARR[i]];
+        RESULT_ARR.push(this.arr[index]);
+      }
 
+      for (let i = 0, j = 0; i < NEW_MESSAGE.length; i++) {
+        if (OBJ_SYMBOL[`${i}`]) {
+          FINNALY_ARR[i] = OBJ_SYMBOL[i]
+        } else {
+          FINNALY_ARR[i] = RESULT_ARR[j];
+          j++;
+        }
+      }
 
-
+      return FINNALY_ARR.reverse().join('');
 
     }
   }
 }
-
-
-// const directMachine = new VigenereCipheringMachine();
-// const reverseMachine = new VigenereCipheringMachine(false);
-
-
-// console.log(reverseMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey'));
-// console.log(directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey'));
-// directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey') //PFLWTJP WQ CIOFMYMI: 1, 2, 3, 4.'
-// reverseMachine.encrypt('attack at dawn!', 'alphonse')
-
-
 
 
 module.exports = {
